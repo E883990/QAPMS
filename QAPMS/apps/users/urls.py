79c37cef1675c6ed4975812217a5f09f6ab75c4b
E883990 <419856935@qq.com>
2022-03-10 14:47:24 +0800

    upload files

--- QAPMS/apps/users/urls.py
+++ QAPMS/apps/users/urls.py
@@ -1,5 +1,5 @@
 '''定义users app的urls路径'''
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'users'
@@ -7,9 +7,9 @@ urlpatterns = [
     # 用户注册的url路径
     path('register/',  views.RegisterView.as_view(), name='register'),
     # 判断用户名是否注册
-    path('usernames/(?P<username>[a-zA-Z0-9 _-]{5,20})/count/', views.UsernameCountView.as_view()),
+    re_path('username/(?P<username>[a-zA-Z0-9 _-]{5,20})/count/', views.UsernameCountView.as_view()),
     # 判断EID是否注册
-    path('EID/(?P<EID>[EH]{1}[0-9]{6})/count/', views.EIDCountView.as_view()),
+    re_path('EID/(?P<EID>[EH]{1}[0-9]{6})/count/', views.EIDCountView.as_view()),
     # 判断EMAIL是否注册
-    path('^email/(?P<email>[a-zA-Z0-9.]+@honeywell.com)/count/', views.EmailCountView.as_view()),
+    re_path('email/(?P<email>[a-zA-Z0-9.]+@honeywell.com)/count/', views.EmailCountView.as_view()),
 ]
